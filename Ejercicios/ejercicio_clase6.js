6d772464e15e4df6d419c2d2de0cbc00a0df3fe0
Bayron Leyton <blm.cbrlock.nrnn@gmail.com>
2022-09-05 21:27:43 -0300

    clase_6

---
+++ Ejercicios/ejercicio_clase6.js
@@ -0,0 +1,39 @@
+// var arr1 = [1,2,3,4,5,6,7,8,9]
+// var arr1 = ['a','b','c']
+// var arr1 = [1,'a',3.23,[1,2,3],{soy:"un objeto"}]
+
+// arr1.push(8)
+// console.log(arr1.length);
+
+// let multi = [[1,2,3],[4,5,6],[7,8,9]]
+// for(i = 0; i <= multi.length-1; ++i){
+//     for(j = 0; j <= multi[i].length-1; ++i){
+//         console.log(multi[i][j])
+//     }
+// }
+
+
+// for (i = 0, i <= arr1.length-1;++i){
+//     console.log(arr1[i]
+// }
+
+const multi = [[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+],[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+],[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+
+]
+];
+
+multi.forEach(multi =>
+}
+
+)
