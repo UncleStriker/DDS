079edf2b65a0404c2d61ceeda05c9cb1b1f2ab51
Bayron Leyton <blm.cbrlock.nrnn@gmail.com>
2022-10-03 21:28:40 -0300

    clase_12

---
+++ Ejercicios/ejercicio_calse12.js
@@ -0,0 +1,3 @@
+var Elemento = document.querySelector(".my_label");
+console.log(Elemento)
+arrElemento.forEach(elemento=>{
---
+++ Ejercicios/script_clase12.js
@@ -0,0 +1,25 @@
+// var arr= ["button", "text","password"];
+
+// for(i=0; i<= 10; i++){
+//     elemento = document.getElementById("elemento")
+//     elemento.type = arr[i % 3]
+//     console.log(i)
+// };,
+
+// while((i<10)){
+//     setInterval(()  => {
+//         elemento = document.getElementById("elemento")
+//         elemento.type = arr[i]
+//         console.log(elemento));
+//     }
+// };
+
+// var arrElemento = document.querySelector(".text_1")
+// console.log(arrElemento);
+// arrElemento.forEach(elemento=>{
+//     elemento.value = Math.ceil(Math.random()*1000)
+// });
+
+var arrElemento = document.querySelector("my_label");
+console.log(arrElemento)
+arrElemento.forEach(elemento=>{
