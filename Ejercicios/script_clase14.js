4375ddad17aad8d8d1f16cf1c00563e819e4bd09
Bayron Leyton <blm.cbrlock.nrnn@gmail.com>
2022-10-13 20:00:54 -0300

    clase_14

---
+++ Ejercicios/script_clase14.js
@@ -0,0 +1,31 @@
+var body = document.body
+var mensajes = [
+    "cambiame el color",
+    "cambiame el backgrunnd",
+    "hazme invisible",
+    "cambiame el cursor",
+    "improvisa y experimenta",
+    "me convienrto en mayuscula"
+];
+
+for(i = 0; i <= 5; i++){
+    label = document.createElement('label')
+    label.innerText = mensajes[i]
+
+    switch(i){
+        case 0: label.style.color = 'red';break;
+        case 1: label.style.backgroundColor  = 'aqua';break;
+        case 2: label.style.display = 'none';break;
+        case 3: label.style.cursor = 'pointer';break;
+        case 4: label.style.textDecorationLine = 'line-through';break;
+        case 5: label.style.textTransform = 'uppercase';break;
+                label.style.color = 'darkgreen';break;
+        default:break;
+
+    }
+    br = document.createElement('br')
+    body.appendChild(label)
+    body.appendChild(br)
+}   
+
+
